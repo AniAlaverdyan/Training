5fef41b75181969e9711fa3c56a340dd740437ef
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-18 10:42:25 +0400

    i added comments

--- Bash/Triple Spacing/triplespacec.sh
+++ Bash/Triple Spacing/triplespacec.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+#check if the given file exists
 if [[ -f "$1" ]]
 then
+	#count the number of lines in the file
 	lines="$( wc -l < $1 )"
 
 	counter=1
 
 	while [ $counter -le $lines ]
+	#print a line back to the output file as well as print 2 blank lines
 	do
 		sed "${counter}q;d" $1>>triple_output.txt
 		echo " ">>triple_output.txt
