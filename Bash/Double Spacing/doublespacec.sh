188fffa4c5d07ac00c54151c27a4acabfe9591eb
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-18 10:37:39 +0400

    i added comments

--- Bash/Double Spacing/doublespacec.sh
+++ Bash/Double Spacing/doublespacec.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ -f "$1" ]]
-then
-	lines="$( wc -l < $1 )"
+#check if the given file exists
+if [[ -f "$1" ]] 
+then 
+	#count the number of lines in the file
+	lines="$( wc -l < $1 )" 
 
 	counter=1
 
 	while [ $counter -le $lines ]
-	do
-		sed "${counter}q;d" $1>>double_output.txt
+	#print a line back to the output file as well as print a blank line
+	do  	
+		sed "${counter}q;d" $1>>double_output.txt 
 		echo " ">>double_output.txt
 		((counter++))
 	done
