01d7c90b6f69f189ed418000a062c60a610abf65
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-18 11:00:27 +0400

    i added comments

--- Bash/Prime numbers/prime_copy.sh
+++ Bash/Prime numbers/prime_copy.sh
@@ -2,8 +2,8 @@
 
 for i in {60000..63000}
 do 
-	m=$(echo "sqrt($i)" | bc -l)
-	int=${m%.*}
+	m=$(echo "sqrt($i)" | bc -l) #square root of i
+	int=${m%.*} #integer part of m
 	p=1
 	for ((k=2; k<=$int; k++))
 	do 
@@ -17,6 +17,6 @@ do
 	done
 	if [ $p -eq 1 ]
 	then
-		printf "%d\t\t" $i>>cprime.txt
+		printf "%d\t\t" $i>>cprime.txt #write the number to file
 	fi
 done
