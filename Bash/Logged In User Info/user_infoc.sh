5dbc1dd5d26816312f8e2f8a4365174168ac7238
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-18 11:10:39 +0400

    i added comments

--- Bash/Logged In User Info/user_infoc.sh
+++ Bash/Logged In User Info/user_infoc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-who | awk '{print $1,$3,$4}'>>user.txt
-a=$(finger | sed '1d' | awk '{print $2}')
-sed -i "s/$/\t$a/" user.txt
+who | awk '{print $1,$3,$4}'>>user.txt #print username, date and time info
+a=$(finger | sed '1d' | awk '{print $2}') #take real names of logged in users
+sed -i "s/$/\t$a/" user.txt #print real names in a separate column
 
 
 
