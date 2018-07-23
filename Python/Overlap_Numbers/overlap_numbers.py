adf46b8337976bf016a898ea7e19b98f8890fdc0
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-23 17:41:55 +0400

    I added try-except.

--- Python/Overlap_Numbers/overlap_numbers.py
+++ Python/Overlap_Numbers/overlap_numbers.py
@@ -5,6 +5,12 @@ with open('overlap1.txt', 'r') as i1, open('overlap2.txt', 'r') as i2, open('res
 	l1 = [int(x) for x in i1.read().split()]
 	l2 = [int(x) for x in i2.read().split()]
 	l = [num1 for num1 in l1 for num2 in l2 if num1 == num2]	
-	o.write(str(set(l)))
-print(l1)
-print(l2)
+	res = set(l)
+	o.write(str(res))
+
+print(res)
+for e in res:
+	print(e)
+
+#print(l1)
+#print(l2)
