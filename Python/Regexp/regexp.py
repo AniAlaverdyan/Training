625fe2fd279b118fcb8dd68d5da1b3ef1f97474e
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-24 17:22:28 +0400

    I added regular expression.

--- Python/Regexp/regexp.py
+++ Python/Regexp/regexp.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
 
-def wordsWithCapLetters():
-	"""
-	Args: 
-		list_of_input_words - list of input file strings
-		list_of_output_words - list of strings for output file
-	"""
+import re
+def wordsWithCapLettersR():
 	with open('words.txt', 'r') as input_file, open('words_with_capital.txt', 'w') as output_file:
-		list_of_input_words = [str(x) for x in input_file.read().split()]		
-		print(list_of_input_words)
-		
-		list_of_output_words = []
-		for word in list_of_input_words:
-			if word[0].isupper():
-				list_of_output_words.append(word)
-		for element in list_of_output_words:
-			print(element, file = output_file)
+		list_of_matches = re.findall(r'\b[A-Z]+[a-z]*[A-Z]*\b', input_file.read())
+		print(list_of_matches)
 
-wordsWithCapLetters()
+wordsWithCapLettersR()
