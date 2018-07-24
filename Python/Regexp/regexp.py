c7f791ed29c35c279036b6704004ffc9fccba00d
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-24 17:44:48 +0400

    list to output file

--- Python/Regexp/regexp.py
+++ Python/Regexp/regexp.py
@@ -5,5 +5,7 @@ def wordsWithCapLettersR():
 	with open('words.txt', 'r') as input_file, open('words_with_capital.txt', 'w') as output_file:
 		list_of_matches = re.findall(r'\b[A-Z]+[a-z]*[A-Z]*\b', input_file.read())
 		print(list_of_matches)
+		for element in list_of_matches:
+			print(element, file = output_file)
 
 wordsWithCapLettersR()
