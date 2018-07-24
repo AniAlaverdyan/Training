b4aae353c68257dbc0fc4a968a4933cceda73a00
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-07-24 09:40:26 +0400

    Changed the print command

--- Python/Overlap_Numbers/overlap_numbers.py
+++ Python/Overlap_Numbers/overlap_numbers.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
-num_lines = 0
-with open('overlap1.txt', 'r') as i1, open('overlap2.txt', 'r') as i2, open('result.txt', 'a') as o:
-	l1 = [int(x) for x in i1.read().split()]
-	l2 = [int(x) for x in i2.read().split()]
-	l = [num1 for num1 in l1 for num2 in l2 if num1 == num2]	
-	res = set(l)
-	o.write(str(res))
-
-print(res)
-for e in res:
-	print(e)
-
-#print(l1)
-#print(l2)
+with open('overlap1.txt', 'r') as input_file1, open('overlap2.txt', 'r') as input_file2, open('result.txt', 'a') as output_file:
+	list_of_input1 = [int(x) for x in input_file1.read().split()]
+	list_of_input2 = [int(x) for x in input_file2.read().split()]
+	list_for_output = [num1 for num1 in list_of_input1 for num2 in list_of_input2 if num1 == num2]	
+	res = set(list_for_output)
+	
+	for element in res:
+		print(element, file=output_file)
