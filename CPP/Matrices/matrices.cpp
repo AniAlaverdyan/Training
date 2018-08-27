ae3515e285e9cbf6ec1a120f827e93c6081e2a88
AniAlaverdyan <ani.soad.alaverdyan@gmail.com>
2018-08-27 18:12:39 +0400

    i added destructor

--- CPP/Matrices/matrices.cpp
+++ CPP/Matrices/matrices.cpp
@@ -9,14 +9,21 @@ private:
     
 public:
     Matrix(int row, int col) {
-    std::cout << "Enter matrix" << std::endl;
-    m_rows = row;
-    m_cols = col;
-    m_array = new int*[m_rows];
-    for (int i = 0; i < m_rows; ++i) {
-        m_array[i] = new int[m_cols];
+        std::cout << "Create matrix" << std::endl;
+        m_rows = row;
+        m_cols = col;
+        m_array = new int*[m_rows];
+        for (int i = 0; i < m_rows; ++i) {
+            m_array[i] = new int[m_cols];
+        }
     }
-    }
+
+    ~Matrix() {
+        for (int i = 0; i < m_rows; ++i) {
+            delete m_array[i];
+        }
+        delete m_array;
+    };
 
     void fillMatrix() {
         for(int i = 0; i < m_rows; i++) {
@@ -29,23 +36,23 @@ public:
     void print() {
         for(int i = 0; i < m_rows; i++) {
             for(int j = 0; j < m_cols; j++) {
-                std::cout << m_array[i][j];
+                std::cout << m_array[i][j] << '\t'; 
             }
             std::cout << std::endl;
         }
         
     }
-    Matrix operator + (Matrix M)
-    { 
+    const Matrix operator + (const Matrix& M)
+    {
         Matrix Sum(m_rows, m_cols);
+        std::cout << "Sum of Matrixes" << std::endl;
         for(int i = 0; i < m_rows; i++) {
 	    for(int j = 0; j < m_cols; j++)
                 {
 		     Sum.m_array[i][j] = m_array[i][j] + M.m_array[i][j];
-		     std::cout << Sum.m_array[i][j] << " ";
 		}
-            std::cout << std::endl;
          }
+         return Sum;
     }
 };
 
@@ -65,5 +72,6 @@ int main()
     M2.fillMatrix();
     
     Matrix M3 = M1 + M2;
+    M3.print();
     return 0;
 }
